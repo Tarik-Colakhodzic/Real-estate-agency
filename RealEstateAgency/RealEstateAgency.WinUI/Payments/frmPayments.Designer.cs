95812dd047045c9d067de200c56af1ba35603f1f
Tarik-Colakhodzic <tarik.colakhodzic@edu.fit.ba>
2022-02-03 23:32:52 +0100

    Filters for payments added

--- RealEstateAgency/RealEstateAgency.WinUI/Payments/frmPayments.Designer.cs
+++ RealEstateAgency/RealEstateAgency.WinUI/Payments/frmPayments.Designer.cs
@@ -30,12 +30,17 @@ namespace RealEstateAgency.WinUI.Payments
         private void InitializeComponent()
         {
             this.dgvPayments = new System.Windows.Forms.DataGridView();
-            this.lblUkupno = new System.Windows.Forms.Label();
             this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Amount = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.CreatedFormatted = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblUkupno = new System.Windows.Forms.Label();
+            this.txtChargeId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCustomerOrTitle = new System.Windows.Forms.TextBox();
+            this.btnTrazi = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPayments)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,21 +54,12 @@ namespace RealEstateAgency.WinUI.Payments
             this.Description,
             this.Status,
             this.CreatedFormatted});
-            this.dgvPayments.Location = new System.Drawing.Point(12, 12);
+            this.dgvPayments.Location = new System.Drawing.Point(12, 86);
             this.dgvPayments.Name = "dgvPayments";
             this.dgvPayments.RowHeadersWidth = 51;
             this.dgvPayments.RowTemplate.Height = 24;
             this.dgvPayments.Size = new System.Drawing.Size(1598, 445);
             this.dgvPayments.TabIndex = 0;
-            // 
-            // lblUkupno
-            // 
-            this.lblUkupno.AutoSize = true;
-            this.lblUkupno.Location = new System.Drawing.Point(1200, 460);
-            this.lblUkupno.Name = "lblUkupno";
-            this.lblUkupno.Size = new System.Drawing.Size(241, 17);
-            this.lblUkupno.TabIndex = 1;
-            this.lblUkupno.Text = "Ukupan iznos uspiješnih transakcija: ";
             // 
             // Id
             // 
@@ -103,11 +99,67 @@ namespace RealEstateAgency.WinUI.Payments
             this.CreatedFormatted.MinimumWidth = 6;
             this.CreatedFormatted.Name = "CreatedFormatted";
             // 
+            // lblUkupno
+            // 
+            this.lblUkupno.AutoSize = true;
+            this.lblUkupno.Location = new System.Drawing.Point(1200, 534);
+            this.lblUkupno.Name = "lblUkupno";
+            this.lblUkupno.Size = new System.Drawing.Size(241, 17);
+            this.lblUkupno.TabIndex = 1;
+            this.lblUkupno.Text = "Ukupan iznos uspiješnih transakcija: ";
+            // 
+            // txtChargeId
+            // 
+            this.txtChargeId.Location = new System.Drawing.Point(12, 40);
+            this.txtChargeId.Name = "txtChargeId";
+            this.txtChargeId.Size = new System.Drawing.Size(204, 22);
+            this.txtChargeId.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Id transakcije";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(222, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Korisnik ili naslov nekretnine";
+            // 
+            // txtCustomerOrTitle
+            // 
+            this.txtCustomerOrTitle.Location = new System.Drawing.Point(222, 40);
+            this.txtCustomerOrTitle.Name = "txtCustomerOrTitle";
+            this.txtCustomerOrTitle.Size = new System.Drawing.Size(411, 22);
+            this.txtCustomerOrTitle.TabIndex = 5;
+            // 
+            // btnTrazi
+            // 
+            this.btnTrazi.Location = new System.Drawing.Point(1495, 29);
+            this.btnTrazi.Name = "btnTrazi";
+            this.btnTrazi.Size = new System.Drawing.Size(115, 33);
+            this.btnTrazi.TabIndex = 7;
+            this.btnTrazi.Text = "Traži";
+            this.btnTrazi.UseVisualStyleBackColor = true;
+            this.btnTrazi.Click += new System.EventHandler(this.btnTrazi_Click);
+            // 
             // frmPayments
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1622, 494);
+            this.ClientSize = new System.Drawing.Size(1622, 565);
+            this.Controls.Add(this.btnTrazi);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCustomerOrTitle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtChargeId);
             this.Controls.Add(this.lblUkupno);
             this.Controls.Add(this.dgvPayments);
             this.Name = "frmPayments";
@@ -128,5 +180,10 @@ namespace RealEstateAgency.WinUI.Payments
         private System.Windows.Forms.DataGridViewTextBoxColumn Description;
         private System.Windows.Forms.DataGridViewTextBoxColumn Status;
         private System.Windows.Forms.DataGridViewTextBoxColumn CreatedFormatted;
+        private System.Windows.Forms.TextBox txtChargeId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCustomerOrTitle;
+        private System.Windows.Forms.Button btnTrazi;
     }
 }
--- RealEstateAgency/RealEstateAgency.WinUI/Payments/frmPayments.cs
+++ RealEstateAgency/RealEstateAgency.WinUI/Payments/frmPayments.cs
@@ -10,6 +10,7 @@ namespace RealEstateAgency.WinUI.Payments
     public partial class frmPayments : Form
     {
         private APIService _servicePayment = new APIService("Payment");
+        List<Model.Payment> payments = new List<Model.Payment>();
 
         public frmPayments()
         {
@@ -21,7 +22,7 @@ namespace RealEstateAgency.WinUI.Payments
         {
             try
             {
-                var payments = await _servicePayment.GetAll<List<Model.Payment>>();
+                payments = await _servicePayment.GetAll<List<Model.Payment>>();
                 dgvPayments.DataSource = payments;
                 lblUkupno.Text += payments.Where(x => x.Status == "succeeded").Sum(x => x.Amount).ToString() + " KM";
             }
@@ -29,6 +30,20 @@ namespace RealEstateAgency.WinUI.Payments
             {
                 MessageBox.Show(Resources.Error_Occured);
             }
+        }
+
+        private void btnTrazi_Click(object sender, EventArgs e)
+        {
+            var query = payments.AsQueryable();
+            if (!string.IsNullOrEmpty(txtChargeId.Text))
+            {
+                query = query.Where(x => x.Id == txtChargeId.Text);
+            }
+            if (!string.IsNullOrEmpty(txtCustomerOrTitle.Text))
+            {
+                query = query.Where(x => x.Description.Contains(txtCustomerOrTitle.Text));
+            }
+            dgvPayments.DataSource = query.ToList();
         }
     }
 }
